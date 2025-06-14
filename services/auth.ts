ecf0c026fbb6cf9c7b3f1dfc0f7f9b8456349d5e
baurzhan <todd-45@mail.ru>
2025-06-14 14:21:25 +0300

    delete api auth 89

--- services/auth.ts
+++ services/auth.ts
@@ -21,13 +21,3 @@ export const register = async (
     return null;
   }
 };
-
-// // export const allProducts = async (): Promise<Product[]> => {
-// //     try {
-// //       const response = await axiosInstance.get<Product[]>(ApiRoutes.PRODUCTS);
-// //       return response.data;
-// //     } catch (error) {
-// //       console.error("API client: ошибка при получении всех продуктов:", error);
-// //       return [];
-// //     }
-// //   };
