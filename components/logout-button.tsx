22da6769ac75a9b75672880ff295c699033d8967
baurzhan <todd-45@mail.ru>
2025-06-16 16:15:16 +0300

    chenge site name 2

--- components/logout-button.tsx
+++ components/logout-button.tsx
@@ -3,13 +3,15 @@
 import { createClient } from "@/lib/supabase/client";
 import { Button } from "@/components/ui/button";
 import { useRouter } from "next/navigation";
+import { useUserStore } from "@/store/auth.store";
 
 export function LogoutButton() {
   const router = useRouter();
-
+  const clearUser = useUserStore((state) => state.clearUser);
   const logout = async () => {
     const supabase = createClient();
     await supabase.auth.signOut();
+    clearUser();
     router.push("/auth/login");
   };
 
